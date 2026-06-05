90f130b51afedcf4953d851f65c2d86ff676ffb5
impl0x <implox.v1@gmail.com>
2026-06-05 21:23:48 +0530

    Added a logger module, seperated config structs and actual structs, defined a init function which checks for errors.

--- server/ratelimit/ratelimit.go
+++ server/ratelimit/ratelimit.go
@@ -1,32 +1,40 @@
 package ratelimit
 
 import (
+	"go-backend/modules/logger"
+	"strings"
 	"time"
 )
-
-
-type FixedWindowCounterConfig struct {
-	MaxRequests    uint16 // per sec
+//? configs
+type FixedWindowCounterConfig struct { // Global rl
+	MaxRequests uint16 // per sec
 }
-type TokenBucketConfig struct {
+type TokenBucketConfig struct { // PerIp rl
 	MaxTokens  uint8
 	RefillRate uint8 // per sec
-	SendRetryAfter bool   // sends the "retry_after" in json response if set to true
+	RetryAfter bool  // sends the "retry_after" in json response, defaults to true
 }
 type RatelimitConfig struct {
-	Global FixedWindowCounterConfig
-	PerIp  TokenBucketConfig
+	Global       FixedWindowCounterConfig
+	PerIp        TokenBucketConfig
 	ErrorMessage string
 }
+
+//? ratelimiters
 type fixedWindowCounter struct {
 	config  FixedWindowCounterConfig
 	counter uint16
 }
 
-
 type tokenBucket struct {
 	config TokenBucketConfig
-	ips    *[]userIp
+	ips    []userIp
+}
+
+type ratelimit struct{
+	global fixedWindowCounter
+	perIp tokenBucket
+	errorMsg string
 }
 
 type userIp struct {
@@ -35,13 +43,22 @@ type userIp struct {
 	lastSeen time.Time
 }
 
+//? main functions
+var ratelimiter ratelimit
 
-func initRatelimiter(config RatelimitConfig){
-	// checks for invalid data
-	// global
-	if config.Global.MaxRequests==0{
-		print("Invalid data")
-		// os.Exit(1)
-		// TODO: implement a good logger here which does fatal
+func Init(config RatelimitConfig) {
+	// checks for invalid config data
+	if config.Global.MaxRequests == 0 {
+		logger.Fatal("Max requests cannot be 0 !", "ratelimit error", "global")
+	} else if config.PerIp.MaxTokens == 0 {
+		logger.Fatal("Max tokens cannot be 0 !", "ratelimit error", "per ip")
+	} else if config.PerIp.RefillRate == 0 {
+		logger.Fatal("Refill rate cannot be 0 !", "ratelimit error", "per ip")
+	} else if strings.TrimSpace(config.ErrorMessage) == "" {
+		logger.Warn("Error message as receivied in config is empty space! Will fall back to the default message", "ratelimit error", "ErrorMessage value")
 	}
+	ratelimiter.global.config=config.Global
+	ratelimiter.perIp.config=config.PerIp
+	ratelimiter.errorMsg=config.ErrorMessage
 }
+
