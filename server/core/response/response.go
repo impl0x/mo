857602bd6069332fac232b49e31d8954ae66c480
impl0x <implox.v1@gmail.com>
2026-06-17 21:54:43 +0530

    added response types, (in progress)

---
+++ server/core/response/response.go
@@ -0,0 +1,37 @@
+package response
+
+import "net/http"
+
+type Response struct {
+	ContentType    ContentType
+	StatusCode     uint16
+	Body           any
+	ResponseWriter http.ResponseWriter
+}
+
+type ContentType struct {
+	headerValue string
+}
+
+var (
+	JSON      = ContentType{"application/json"}
+	Text      = ContentType{"text/plain"}
+	NoContent = ContentType{}
+)
+
+func NewResponse(cType ContentType, body any, w http.ResponseWriter) *Response {
+	return &Response{
+		ContentType:    cType,
+		StatusCode:     200, // default, if user wants he can change the field after initialization.
+		Body:           body,
+		ResponseWriter: w,
+	}
+}
+
+func (r *Response) setReqMetaData() {
+	if r.ContentType!=NoContent{
+		r.ResponseWriter.Header().Set("Content-Type", r.ContentType.headerValue)
+	}
+	r.ResponseWriter.WriteHeader(int(r.StatusCode))
+
+}
