3daf7fce971e49b104aeeb8d76c97a9c55dfcbc8
impl0x <implox.v1@gmail.com>
2026-06-20 21:27:09 +0530

    made responses

--- context.go
+++ context.go
@@ -1,6 +1,11 @@
 package mo
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/impl0x/mo/modules/logger"
+)
 
 type Context struct {
 	request  *http.Request
@@ -8,6 +13,52 @@ type Context struct {
 	Mo       *Mo
 }
 
-func (c *Context) JSON(){
+func (c *Context) writeContentType(value string) {
+	header := c.response.Header()
+	if header.Get(HeaderContentType) == "" {
+		header.Set(HeaderContentType, value)
+	}
+}
+
+// Redirect redirects the request to a provided URL with status code.
+func (c *Context) Redirect(code int, url string)  {
+	if code < 300 || code > 308 {
+		c.Mo.HTTPErrorHandler(c,ErrInternalServerError)
+		return
+	}
+	c.response.Header().Set(HeaderLocation, url)
+	c.response.WriteHeader(code)
 	
+}
+
+// NoContent sends a response with no body and a status code.
+func (c *Context) NoContent(code int) {
+	c.response.WriteHeader(code)
+}
+
+// Blob sends a blob response with status code and content type.
+func (c *Context) Blob(code int, contentType string, b []byte) {
+	c.writeContentType(contentType)
+	c.response.WriteHeader(code)
+	writeResp(c.response, b)
+}
+
+// JSON sends a JSON response with status code.
+func (c *Context) JSON(code int, target any) {
+	v,err:=json.Marshal(target)
+	if err != nil {
+		c.Mo.HTTPErrorHandler(c,ErrInternalServerError)
+		return 
+	}
+	c.writeContentType(MIMEApplicationJSON)
+	c.response.WriteHeader(code)
+	writeResp(c.response,v)
+}
+
+func writeResp(resp http.ResponseWriter,b []byte){
+	_, err := resp.Write(b)
+	if err != nil {
+		logger.Error("Client disconnected! couldn't write response")
+		return 
+	}
 }
--- test/t.go
+++ test/t.go
@@ -1,11 +1,19 @@
 package main
 
-import "github.com/impl0x/mo"
+import "github.com/labstack/echo/v5"
 
-// import "github.com/labstack/echo/v5"
+type Example struct {
+	FieldOne string   `json:"field_one"`
+	FieldTwo chan int `json:"field_two"` // bad field
+}
 
 func main() {
-	n:=mo.New()
-	n.GET("/",func(c *mo.Context) error {println("Root test");return nil})
-	n.Start(":8080")
+	test := echo.New()
+	test.GET("/", handler)
+	test.Start(":8080")
+}
+
+func handler(c *echo.Context) error {
+	c.JSON(204, make(chan int)) // will return an error
+	return nil
 }
