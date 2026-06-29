6c11be271444e2380382dc367640fda7b3b8b867
impl0x <implox.v1@gmail.com>
2026-06-29 18:59:43 +0530

    added logging middleware

---
+++ middlewares/logger.go
@@ -0,0 +1,31 @@
+package middlewares
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/impl0x/mo"
+	"github.com/impl0x/mo/modules/logger"
+)
+
+func Logger(next mo.HandlerFunc) mo.HandlerFunc {
+	return func(c *mo.Context) error {
+		req := c.Request()
+		v := fmt.Sprintf("%v -> %v", req.RemoteAddr, req.URL.Path)
+		switch req.Method {
+		case http.MethodGet:
+			logger.Get(v)
+		case http.MethodPost:
+			logger.Post(v)
+		case http.MethodPatch:
+			logger.Patch(v)
+		case http.MethodPut:
+			logger.Put(v)
+		case http.MethodDelete:
+			logger.Delete(v)
+		default:
+			logger.Default(req.Method, v)
+		}
+		return next(c)
+	}
+}
--- modules/logger/custom.go
+++ modules/logger/custom.go
@@ -1,5 +1,7 @@
 package logger
 
+import "strings"
+
 var mo = Level{
 	Color: Colors["light_blue"],
 	Label: "Mo",
@@ -7,4 +9,57 @@ var mo = Level{
 
 func Mo(v ...string) {
 	log(mo, v)
+}
+
+var get = Level{
+	Color: Colors["cyan"],
+	Label: "GET",
+}
+
+var post = Level{
+	Color: Colors["orange"],
+	Label: "POST",
+}
+
+var patch = Level{
+	Color: Colors["purple"],
+	Label: "PATCH",
+}
+
+var put = Level{
+	Color: Colors["magenta"],
+	Label: "PATCH",
+}
+
+var delete = Level{
+	Color: Colors["yellow"],
+	Label: "DELETE",
+}
+
+func Get(v ...string) {
+	log(get, v)
+}
+
+func Post(v ...string) {
+	log(post, v)
+}
+
+func Put(v ...string) {
+	log(put, v)
+}
+
+func Patch(v ...string) {
+	log(patch, v)
+}
+
+func Delete(v ...string) {
+	log(delete, v)
+}
+
+func Default(method string, v ...string) {
+	log(Level{
+		Color: Colors["grey"],
+		Label: strings.ToUpper(method)},
+		v,
+	)
 }
