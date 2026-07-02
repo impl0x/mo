932eb304a7cc1cd8762a001afa545c59a6d16f03
impl0x <implox.v1@gmail.com>
2026-07-02 20:27:29 +0530

    added recovery middleware

--- middlewares/logger.go
+++ middlewares/logger.go
@@ -8,24 +8,26 @@ import (
 	"github.com/impl0x/mo/modules/logger"
 )
 
-func Logger(next mo.HandlerFunc) mo.HandlerFunc {
-	return func(c *mo.Context) error {
-		req := c.Request()
-		v := fmt.Sprintf(`%v -> "%v"`, req.RemoteAddr, req.URL.Path)
-		switch req.Method {
-		case http.MethodGet:
-			logger.Get(v)
-		case http.MethodPost:
-			logger.Post(v)
-		case http.MethodPatch:
-			logger.Patch(v)
-		case http.MethodPut:
-			logger.Put(v)
-		case http.MethodDelete:
-			logger.Delete(v)
-		default:
-			logger.Default(req.Method, v)
+func Logger() mo.Middleware {
+	return func(next mo.HandlerFunc) mo.HandlerFunc {
+		return func(c *mo.Context) error {
+			req := c.Request()
+			v := fmt.Sprintf(`%v -> "%v"`, req.RemoteAddr, req.URL.Path)
+			switch req.Method {
+			case http.MethodGet:
+				logger.Get(v)
+			case http.MethodPost:
+				logger.Post(v)
+			case http.MethodPatch:
+				logger.Patch(v)
+			case http.MethodPut:
+				logger.Put(v)
+			case http.MethodDelete:
+				logger.Delete(v)
+			default:
+				logger.Default(req.Method, v)
+			}
+			return next(c)
 		}
-		return next(c)
 	}
 }
---
+++ middlewares/recover.go
@@ -0,0 +1,21 @@
+package middlewares
+
+import (
+
+
+	"github.com/impl0x/mo"
+
+)
+
+func Recover() mo.Middleware {
+	return func(next mo.HandlerFunc) mo.HandlerFunc {
+		return func(c *mo.Context) error {
+			defer func() {
+				if err := Recover(); err != nil {
+					c.JSON(500,mo.ErrInternalServerError.JsonFormat())
+				}
+			}()
+			return next(c)
+		}
+	}
+}
