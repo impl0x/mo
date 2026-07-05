498deaba2fc2c9887d259a7dcbbcf5dc2911950b
impl0x <implox.v1@gmail.com>
2026-07-05 22:26:26 +0530

    router todo changes

--- router.go
+++ router.go
@@ -12,7 +12,7 @@ type Route struct {
 	Middlewares []Middleware // returns a copy of slice, don't mutate
 }
 
-// o(n)
+// o(n) and doesn't support dynamic routing
 type BasicRouter struct {
 	Routes []*Route
 }
@@ -36,77 +36,4 @@ func (r *BasicRouter) Find(path string, method string) (*Route, HttpErrorInterfa
 	return nil, ErrNotFound
 }
 
-// type RadixRouter struct {
-// 	root *node
-// }
-
-// type pathKind uint8
-
-// const (
-// 	static pathKind = iota
-// 	param
-// 	wildcard
-// )
-
-// type methodHandlers struct {
-// 	get, post, put, patch, delete, options, head HandlerFunc
-// }
-
-// type node struct {
-// 	part           string
-// 	methodHandlers *methodHandlers
-// 	kind           pathKind
-// 	parent         *node
-// 	child          *node
-// }
-
-// func NewRadixRouter() *RadixRouter {
-// 	return &RadixRouter{
-// 		root: &node{
-// 			part:           "/",
-// 			methodHandlers: nil,
-// 			kind:           static,
-// 			parent:         nil,
-// 			child:          nil,
-// 		},
-// 	}
-// }
-
-// func newMethodHandler(method string, handler HandlerFunc) *methodHandlers{
-// 	mh:=methodHandlers{}
-// 	switch method {
-// 	case http.MethodGet:
-// 		mh.get = handler
-// 	case http.MethodPost:
-// 		mh.post = handler
-// 	case http.MethodPut:
-// 		mh.put = handler
-// 	case http.MethodPatch:
-// 		mh.patch = handler
-// 	case http.MethodDelete:
-// 		mh.delete = handler
-// 	case http.MethodOptions:
-// 		mh.options = handler
-// 	case http.MethodHead:
-// 		mh.head = handler
-// 	}
-// 	return &mh
-// }
-
-// func (r *RadixRouter) Add(ro Route) {
-// 	pathSplits:=strings.Split(ro.Path, "/")
-// 	if r.root.child == nil {
-// 		var n *node
-// 		for _,v:=range pathSplits{
-// 			n=&node{
-// 				part: v,
-// 				// kind: ,,
-// 			}
-// 		}
-// 		r.root.child = &node{
-// 			part:           ro.Path,
-// 			methodHandlers: newMethodHandler(ro.Method,ro.Handler),
-// 			// kind:
-// 		}
-// 	}
-// }
+// todo: figure out a way to add a dynamic router which supports dynamic and wildcard paths
