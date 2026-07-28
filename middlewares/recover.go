9309c1124132095df2a8872ed1c837967d61f3d4
impl0x <implox.v1@gmail.com>
2026-07-28 22:08:15 +0530

    bug fix

--- middlewares/recover.go
+++ middlewares/recover.go
@@ -7,14 +7,16 @@ import (
 	"github.com/impl0x/mo"
 	"github.com/impl0x/mo/modules/logger"
 )
-
+// set logError to true to log the panics in your code. it is recommended to do so.
 func Recover(logError bool) mo.Middleware {
 	return func(next mo.HandlerFunc) mo.HandlerFunc {
 		return func(c *mo.Context) error {
 			defer func() {
 				if err := recover(); err != nil {
-					logger.Panic(fmt.Sprintf("%v", err))
-					println("[STACK TRACE]\n" + string(debug.Stack()))
+					if logError{
+						logger.Panic(fmt.Sprintf("%v", err))
+						println("[STACK TRACE]\n" + string(debug.Stack()))
+					}
 					c.JSON(500, mo.ErrInternalServerError.JsonFormat())
 				}
 			}()
