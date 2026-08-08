aef4429acaec3f546a2f34a0b8400793fe736d6e
impl0x <implox.v1@gmail.com>
2026-08-08 19:17:54 +0530

    grouped all the vars

--- validator/rules.go
+++ validator/rules.go
@@ -31,47 +31,49 @@ const ( // these are all nonEq rules
 	dive validatorRule = "dive"
 )
 
-var emailRx = regexRule{
-	email,
-	regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9]*[a-zA-Z][a-zA-Z0-9]*$`),
-}
-var uuidRx = regexRule{
-	uuid,
-	regexp.MustCompile(`^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$`),
-}
-var urlRx = regexRule{
-	url,
-	regexp.MustCompile(`^https?:\/\/(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(?::\d{1,5})?(?:\/[^\s]*)?$`),
-}
-var e164Rx = regexRule{
-	e164,
-	regexp.MustCompile(`^\+[1-9]\d{1,14}$`),
-}
+var (
+	emailRx = regexRule{
+		email,
+		regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9]*[a-zA-Z][a-zA-Z0-9]*$`),
+	}
+	uuidRx = regexRule{
+		uuid,
+		regexp.MustCompile(`^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$`),
+	}
+	urlRx = regexRule{
+		url,
+		regexp.MustCompile(`^https?:\/\/(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(?::\d{1,5})?(?:\/[^\s]*)?$`),
+	}
+	e164Rx = regexRule{
+		e164,
+		regexp.MustCompile(`^\+[1-9]\d{1,14}$`),
+	}
 
-var alphaRx = regexRule{
-	alpha,
-	regexp.MustCompile(`^[a-zA-Z]+$`),
-}
+	alphaRx = regexRule{
+		alpha,
+		regexp.MustCompile(`^[a-zA-Z]+$`),
+	}
 
-var alphanumRx = regexRule{
-	alphanum,
-	regexp.MustCompile(`^[a-zA-Z0-9]+$`),
-}
+	alphanumRx = regexRule{
+		alphanum,
+		regexp.MustCompile(`^[a-zA-Z0-9]+$`),
+	}
 
-var numericRx = regexRule{
-	numeric,
-	regexp.MustCompile(`^[0-9]+$`),
-}
+	numericRx = regexRule{
+		numeric,
+		regexp.MustCompile(`^[0-9]+$`),
+	}
 
-var ipv4Rx = regexRule{
-	ipv4,
-	regexp.MustCompile(`^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.){3}(25[0-5]|(2[0-4]|1\d|[1-9]|)\d)$`),
-}
+	ipv4Rx = regexRule{
+		ipv4,
+		regexp.MustCompile(`^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.){3}(25[0-5]|(2[0-4]|1\d|[1-9]|)\d)$`),
+	}
 
-var ipv6Rx = regexRule{
-	ipv6,
-	regexp.MustCompile(`^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$`),
-}
+	ipv6Rx = regexRule{
+		ipv6,
+		regexp.MustCompile(`^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$`),
+	}
+)
 
 func (r *regexRule) validate(vd *validator) ValidationError {
 	if vd.f.kind != reflect.String {
