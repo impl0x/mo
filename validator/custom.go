303064ff5183b826f42a36292d85ff7b6abe3ee7
implox <implox.v1@gmail.com>
2026-08-25 00:51:22 +0530

    comment changes

--- validator/custom.go
+++ validator/custom.go
@@ -1,8 +1,8 @@
 package validator
 
+// Interface satisfying the custom validation logic, made to store generic structs in a map without insantiation
 type CustomValidator interface {
 	Validate(f *field, parent string, value any) ValidationError
-	Error() string
 }
 
 var customValidations = map[string]CustomValidator{}
@@ -23,16 +23,16 @@ func (cd customValidator[T]) Validate(f *field, parent string, value any) Valida
 	}
 	return newFieldValidateError(cd.errMsg, "", parent, *f)
 }
-func (cd customValidator[T]) Error() string {
-	return cd.errMsg
-}
 
 // Adds a custom validation tag with a function determining the validation
 //
-// 	- tag: the tag name used in the actual struct tags, such as `validator:"tagname"`
-// 	- errorMessage: the error message to be returned if validation fails
-// 	- fn: the function which performs the logical operation on the field value deciding the validation for it, return true to pass and false to fail validation.
+//   - tag: the tag name used in the actual struct tags, such as `validator:"tagname"`
+//   - errorMessage: the error message to be returned if validation fails
+//   - fn: the function which performs the logical operation on the field value deciding the validation for it, return true to pass and false to fail validation.
+//
 // the field type must be same as the generic type in the func fn. Otherwise a user error is returned by default.
+//
+// This adds it to a global non mutexed map of tag and functions, do not add at runtime
 func AddCustomValidation[T any](tag string, errorMessage string, fn func(value T) bool) {
 	customValidations[tag] = customValidator[T]{errorMessage, fn}
 }
