cccd808263d9dc5c8fd6ea474277703f152a6f75
implox <implox.v1@gmail.com>
2026-08-25 22:36:49 +0530

    Update custom validation function to return error value instead of bool

--- validator/custom.go
+++ validator/custom.go
@@ -8,8 +8,7 @@ type CustomValidator interface {
 var customValidations = map[string]CustomValidator{}
 
 type customValidator[T any] struct {
-	errMsg string
-	fn     func(value T) bool
+	fn func(value T) error
 }
 
 func (cd customValidator[T]) Validate(f *field, parent string, value any) ValidationError {
@@ -17,22 +16,21 @@ func (cd customValidator[T]) Validate(f *field, parent string, value any) Valida
 	if !ok {
 		return newUserError("Type does not match according to the custom validation generic type registered", f.fieldName, parent)
 	}
-	ok = cd.fn(val)
-	if ok {
-		return nil
+	err := cd.fn(val)
+	if err != nil {
+		return newFieldValidateError(err.Error(), "", parent, *f)
 	}
-	return newFieldValidateError(cd.errMsg, "", parent, *f)
+	return nil
 }
 
 // Adds a custom validation tag with a function determining the validation
 //
 //   - tag: the tag name used in the actual struct tags, such as `validator:"tagname"`
-//   - errorMessage: the error message to be returned if validation fails
-//   - fn: the function which performs the logical operation on the field value deciding the validation for it, return true to pass and false to fail validation.
+//   - fn: the function which performs the logical operation on the field value deciding the validation for it, return an error with to pass and nil to fail validation, remember that the Error method ont he error instance will be used as the final error message
 //
 // the field type must be same as the generic type in the func fn. Otherwise a user error is returned by default.
 //
 // This adds it to a global non mutexed map of tag and functions, do not add at runtime
-func AddCustomValidation[T any](tag string, errorMessage string, fn func(value T) bool) {
-	customValidations[tag] = customValidator[T]{errorMessage, fn}
+func AddCustomValidation[T any](tag string, fn func(value T) error) {
+	customValidations[tag] = customValidator[T]{fn}
 }
